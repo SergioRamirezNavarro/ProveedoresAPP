b3106c71561728ebf4cee97c3058678fa1d4a970
SergioRamirezNavarro <ramireznavarro.sergio@gmail.com>
2024-01-16 16:43:49 +0100

    com.ceica modelo proveedor

---
+++ src/controladores/Proveedor.java
@@ -0,0 +1,68 @@
+package controladores;
+
+public class Proveedor
+{
+    private int ID;
+    private String cif,nombre,direccion,localidad,provincia;
+
+    public Proveedor()
+    {
+
+    }
+
+    public Proveedor(java.lang.String cif, java.lang.String nombre)
+    {
+        this.cif = cif;
+        this.nombre = nombre;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String toString() {
+        return "Proveedor{" +
+                "ID=" + ID +
+                ", cif='" + cif + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", direccion='" + direccion + '\'' +
+                ", localidad='" + localidad + '\'' +
+                ", provincia='" + provincia + '\'' +
+                '}';
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+    public java.lang.String getCif() {
+        return cif;
+    }
+    public void setCif(java.lang.String cif) {
+        this.cif = cif;
+    }
+    public java.lang.String getNombre() {
+        return nombre;
+    }
+    public void setNombre(java.lang.String nombre) {
+        this.nombre = nombre;
+    }
+    public java.lang.String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(java.lang.String direccion) {
+        this.direccion = direccion;
+    }
+    public java.lang.String getLocalida() {
+        return localidad;
+    }
+    public void setLocalidad(java.lang.String localida) {
+        this.localidad = localida;
+    }
+    public java.lang.String getProvincia() {
+        return provincia;
+    }
+    public void setProvincia(java.lang.String provincia) {
+        this.provincia = provincia;
+    }
+}
